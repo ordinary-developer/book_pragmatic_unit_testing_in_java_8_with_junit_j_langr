ed7f6a88928eaf04103b87487a66c754a300b137
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-10-17 14:29:46 +0300

    edit the test to contain more sense data
    edit the test to contain more correct data
    for testing the class
    
    after the commit
    - the test will pass

--- ScoreCollectionTest.java
+++ ScoreCollectionTest.java
@@ -1,10 +1,20 @@
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.*;
 
 public class ScoreCollectionTest {
 
     @Test
-    public void test() { 
-        fail("Not yet implemented");
+    public void answersArithmeticMeanOfTwoNumbers() {
+        // arrange
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // act
+        int actualResult = collection.arithmeticMean();
+
+        // assert
+        assertThat(actualResult, equalTo(6));
     }
 }
